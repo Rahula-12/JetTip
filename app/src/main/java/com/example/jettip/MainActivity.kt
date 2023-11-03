c26ed5dce0a3f89578fe4180a82d5663821c63d5
rahul <ra9853044@gmail.com>
2023-11-03 23:19:18 +0530

    improved ui

--- app/src/main/java/com/example/jettip/MainActivity.kt
+++ app/src/main/java/com/example/jettip/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jettip
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ScrollState
@@ -51,12 +52,39 @@ import java.security.spec.ECField
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("OnCreate","OnCreate called")
         setContent {
             JetTipTheme {
                 MainScreen()
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("on","OnStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("on","onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("on","onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("on","onStop called")
+    }
+
+    override fun onDestroy() {
+        Log.d("on","onDestroy called")
+        super.onDestroy()
+    }
+
 }
 
 //@Preview
@@ -138,169 +166,175 @@ fun SetParameters(
     modifier: Modifier=Modifier,
     updateBillToShow:(Float)->Unit={}
 ) {
-    Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(
-                start = 10.dp,
-                end = 10.dp
-            ),
-        shape = RoundedCornerShape(10.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White
-        ),
-        elevation = CardDefaults.cardElevation(
-            5.dp
-        )
-    ) {
-        Column(
+    var currBillString = rememberSaveable {
+        mutableStateOf("")
+    }
+        Card(
             modifier = modifier
                 .fillMaxWidth()
                 .padding(
                     start = 10.dp,
-                    end = 10.dp,
-                    top = 10.dp
-                )
-        ) {
-            var currBill by rememberSaveable {
-                mutableStateOf(0f)
-            }
-            var currBillString =rememberSaveable {
-                mutableStateOf("")
-            }
-            var numberOfPersons by rememberSaveable {
-                mutableStateOf(1)
-            }
-            var tipPercent by rememberSaveable {
-                mutableStateOf(0f)
-            }
-            OutlinedTextField(
-                value = currBillString.value,
-                onValueChange = {
-                    currBillString.value = it
-                    try{
-                        currBill=currBillString.value.toFloat()
-                        if(numberOfPersons!=0) updateBillToShow((currBill+(tipPercent*currBill)/100f)/numberOfPersons)
-                    }
-                    catch (_:Exception) {
-
-                    }
-
-                },
-                modifier=modifier.fillMaxWidth(),
-                leadingIcon = {
-                    Text(text = "$")
-                },
-                label={
-                    Text(text = "Enter Bill")
-                },
-                placeholder = {
-                    Text(text = "Enter Bill")
-                },
-                keyboardOptions= KeyboardOptions(
-                    keyboardType = KeyboardType.Decimal
-                )
+                    end = 10.dp
+                ),
+            shape = RoundedCornerShape(10.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            elevation = CardDefaults.cardElevation(
+                5.dp
             )
-            Row(
-                modifier= modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = 10.dp
-                    )
-            ) {
-                Text(
-                    text = "Split",
-                    modifier=modifier.weight(1f)
-                )
-                Row(
-                    horizontalArrangement = Arrangement.Start,
-                    modifier =  modifier.weight(1f)
-                ) {
-                    Surface(
-                        shape = CircleShape,
-                        shadowElevation=2.5.dp,
-                        modifier = modifier.clickable {
-                            numberOfPersons++
-                            updateBillToShow((currBill+(tipPercent*currBill)/100f)/numberOfPersons)
-                        }
-                    ){
-                        Box(
-                            contentAlignment = Alignment.Center
-                        ){
-                            Icon(
-                                painter = painterResource(id = R.drawable.plus),
-                                contentDescription ="Add",
-                                modifier=modifier.size(25.dp)
-                            )
-                        }
-                    }
-                    Text(
-                        text =  numberOfPersons.toString(),
-                        modifier=modifier.padding(
-                            start = 10.dp,
-                            end=10.dp
-                        ),
-                        fontSize = TextUnit(20f,TextUnitType.Sp)
-                    )
-                    Surface(
-                        shape = CircleShape,
-                        shadowElevation=2.5.dp,
-                        modifier = modifier.clickable {
-                            if(numberOfPersons>1)   numberOfPersons--
-                            updateBillToShow((currBill+(tipPercent*currBill)/100f)/numberOfPersons)
-                        }
-                    ){
-                        Box(
-                            contentAlignment = Alignment.Center
-                        ){
-                            Icon(
-                                painter = painterResource(id = R.drawable.minus),
-                                contentDescription ="Remove",
-                                 modifier=modifier.size(25.dp)
-                            )
-                        }
-                    }
-                }
-            }
-            Row(
-                modifier= modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = 10.dp
-                    )
-            ) {
-                Text(
-                    text = "Tip",
-                    modifier=modifier.weight(1.5f)
-                )
-                Text(
-                    text = "$${"%.2f".format((currBill*tipPercent)/100f)}",
-                    modifier=modifier.weight(1f)
-                )
-            }
-            Text(
-                text = "${"%.2f".format(tipPercent)}%",
+        ) {
+            Column(
                 modifier = modifier
                     .fillMaxWidth()
                     .padding(
+                        start = 10.dp,
+                        end = 10.dp,
                         top = 10.dp
                     )
-                ,
-                textAlign = TextAlign.Center
-            )
-            Slider(
-                value = tipPercent,
-                onValueChange = {
-                    tipPercent = it
-                    updateBillToShow((currBill+(tipPercent*currBill)/100f)/numberOfPersons)
-                                },
-                colors = SliderDefaults.colors(
-                    thumbColor = MaterialTheme.colorScheme.secondary,
-                    activeTrackColor = MaterialTheme.colorScheme.secondary,
-                    inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
-                ),
-                valueRange = 0f..100f
-            )
+            ) {
+                var currBill by rememberSaveable {
+                    mutableStateOf(0f)
+                }
+                var numberOfPersons by rememberSaveable {
+                    mutableStateOf(1)
+                }
+                var tipPercent by rememberSaveable {
+                    mutableStateOf(0f)
+                }
+                OutlinedTextField(
+                    value = currBillString.value,
+                    onValueChange = {
+                        currBillString.value = it
+                        try {
+                            currBill = currBillString.value.toFloat()
+                            if (numberOfPersons != 0) updateBillToShow((currBill + (tipPercent * currBill) / 100f) / numberOfPersons)
+                        } catch (_: Exception) {
+
+                        }
+
+                    },
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(
+                            bottom = 10.dp
+                        )
+                    ,
+                    leadingIcon = {
+                        Text(text = "$")
+                    },
+                    label = {
+                        Text(text = "Enter Bill")
+                    },
+//                placeholder = {
+//                    Text(text = "Enter Bill")
+//                },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Decimal
+                    )
+                )
+                if (currBillString.value != "") {
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(
+                                top = 10.dp
+                            )
+                    ) {
+                        Text(
+                            text = "Split",
+                            modifier = modifier.weight(1f)
+                        )
+                        Row(
+                            horizontalArrangement = Arrangement.Start,
+                            modifier = modifier.weight(1f)
+                        ) {
+                            Surface(
+                                shape = CircleShape,
+                                shadowElevation = 2.5.dp,
+                                modifier = modifier.clickable {
+                                    numberOfPersons++
+                                    updateBillToShow((currBill + (tipPercent * currBill) / 100f) / numberOfPersons)
+                                }
+                            ) {
+                                Box(
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.plus),
+                                        contentDescription = "Add",
+                                        modifier = modifier.size(25.dp)
+                                    )
+                                }
+                            }
+                            Text(
+                                text = numberOfPersons.toString(),
+                                modifier = modifier.padding(
+                                    start = 10.dp,
+                                    end = 10.dp
+                                ),
+                                fontSize = TextUnit(20f, TextUnitType.Sp)
+                            )
+                            Surface(
+                                shape = CircleShape,
+                                shadowElevation = 2.5.dp,
+                                modifier = modifier.clickable {
+                                    if (numberOfPersons > 1) numberOfPersons--
+                                    updateBillToShow((currBill + (tipPercent * currBill) / 100f) / numberOfPersons)
+                                }
+                            ) {
+                                Box(
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.minus),
+                                        contentDescription = "Remove",
+                                        modifier = modifier.size(25.dp)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(
+                                top = 10.dp
+                            )
+                    ) {
+                        Text(
+                            text = "Tip",
+                            modifier = modifier.weight(1.5f)
+                        )
+                        Text(
+                            text = "$${"%.2f".format((currBill * tipPercent) / 100f)}",
+                            modifier = modifier.weight(1f)
+                        )
+                    }
+                    Text(
+                        text = "${"%.2f".format(tipPercent)}%",
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(
+                                top = 10.dp
+                            ),
+                        textAlign = TextAlign.Center
+                    )
+                    Slider(
+                        value = tipPercent,
+                        onValueChange = {
+                            tipPercent = it
+                            updateBillToShow((currBill + (tipPercent * currBill) / 100f) / numberOfPersons)
+                        },
+                        colors = SliderDefaults.colors(
+                            thumbColor = MaterialTheme.colorScheme.secondary,
+                            activeTrackColor = MaterialTheme.colorScheme.secondary,
+                            inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                        ),
+                        valueRange = 0f..100f,
+                        steps = 7
+                    )
+                }
+            }
         }
     }
-}
